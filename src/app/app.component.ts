94e00364637f95091210b073bee626b57ad04568
Luna KF <lunakhanjifustek@gmail.com>
2025-01-27 19:04:18 +0100

    Arreglado EN CLASE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedMenuUnroutedComponent } from "./component/shared/shared.menu.unrouted/shared.menu.unrouted.component";
-import { SharedHomeRoutedComponent } from "./component/shared/shared.home.routed/shared.home.routed.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedMenuUnroutedComponent, SharedHomeRoutedComponent],
+  imports: [RouterOutlet, SharedMenuUnroutedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
